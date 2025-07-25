517e3c93db66239bd86665d07800e69b226f83a7
LinuxChata <linuxchata@hotmail.com>
2025-07-25 20:08:00 +0200

    Set residentKey to preferred for attestation flow (#5)
    Set residentKey to preferred for attestation flow

--- src/Shark.Fido2.Portal/wwwroot/js/ndc/fido2-ndc-attestation.js
+++ src/Shark.Fido2.Portal/wwwroot/js/ndc/fido2-ndc-attestation.js
@@ -6,7 +6,12 @@ async function requestCreateCredentialOptions(username, displayName) {
     const optionsRequest = {
         username: username,
         displayName: displayName,
-        attestation: 'direct'
+        attestation: 'direct',
+        authenticatorSelection: {
+            residentKey: 'preferred',
+            userVerification: 'preferred',
+            requireResidentKey: false
+        }
     };
 
     const options = await fetchAttestationOptions(optionsRequest);
@@ -30,6 +35,7 @@ async function createCredential(options) {
                 type: param.type,
                 alg: param.alg,
             })),
+            authenticatorSelection: options.authenticatorSelection,
             challenge: toUint8Array(options.challenge),
             excludeCredentials: options.excludeCredentials.map(credential => ({
                 id: toUint8Array(credential.id),
