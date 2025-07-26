1d43493de4cd1fde91dc62ec33a485cb5597b8d6
LinuxChata <linuxchata@hotmail.com>
2025-07-26 15:19:40 +0200

    Add highlight for nav links (#9)
    Add highlight for nav links

---
+++ src/Shark.Fido2.Portal/wwwroot/js/shared/nav-highlight.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.nav-link');
+
+    links.forEach(link => {
+        link.addEventListener('touchstart', function () {
+            links.forEach(l => l.classList.remove('hovered'));
+            this.classList.add('hovered');
+        });
+    });
+});
