202c694cb6f10ee8591b9f02fcafabba6b197aaa
linuxchata <linuxchata@hotmail.com>
2026-02-06 08:17:05 +0100

    Add logging of non-PII

--- src/Shark.Fido2.Portal/Services/CredentialService.cs
+++ src/Shark.Fido2.Portal/Services/CredentialService.cs
@@ -6,14 +6,30 @@ namespace Shark.Fido2.Portal.Services;
 public sealed class CredentialService : ICredentialService
 {
     private readonly ICredentialRepository _credentialRepository;
+    private readonly ILogger<CredentialService> _logger;
 
-    public CredentialService(ICredentialRepository credentialRepository)
+    public CredentialService(
+        ICredentialRepository credentialRepository,
+        ILogger<CredentialService> logger)
     {
         _credentialRepository = credentialRepository;
+        _logger = logger;
     }
 
     public async Task<Credential?> Get(byte[] id, CancellationToken cancellationToken)
     {
-        return await _credentialRepository.Get(id, cancellationToken);
+        var credential = await _credentialRepository.Get(id, cancellationToken);
+
+        if (credential is not null)
+        {
+            _logger.LogInformation(
+                "The credential '{CredentialId}' is found. Created at {CreatedAt}, updated at {UpdatedAt}, last used at {LastUsedAt}",
+                credential.CredentialId,
+                credential.CreatedAt,
+                credential.UpdatedAt,
+                credential.LastUsedAt);
+        }
+
+        return credential;
     }
 }
