52e4f9efec7c357c0d5e5f80596e7ebdd1151eec
linuxchata <linuxchata@hotmail.com>
2026-02-06 08:38:15 +0100

    Disable caching of the pages

--- src/Shark.Fido2.Portal/Services/CredentialService.cs
+++ src/Shark.Fido2.Portal/Services/CredentialService.cs
@@ -6,30 +6,14 @@ namespace Shark.Fido2.Portal.Services;
 public sealed class CredentialService : ICredentialService
 {
     private readonly ICredentialRepository _credentialRepository;
-    private readonly ILogger<CredentialService> _logger;
 
-    public CredentialService(
-        ICredentialRepository credentialRepository,
-        ILogger<CredentialService> logger)
+    public CredentialService(ICredentialRepository credentialRepository)
     {
         _credentialRepository = credentialRepository;
-        _logger = logger;
     }
 
-    public async Task<Credential?> Get(byte[] id, CancellationToken cancellationToken)
+    public Task<Credential?> Get(byte[] id, CancellationToken cancellationToken)
     {
-        var credential = await _credentialRepository.Get(id, cancellationToken);
-
-        if (credential is not null)
-        {
-            _logger.LogInformation(
-                "The credential '{CredentialId}' is found. Created at {CreatedAt}, updated at {UpdatedAt}, last used at {LastUsedAt}",
-                credential.CredentialId,
-                credential.CreatedAt,
-                credential.UpdatedAt,
-                credential.LastUsedAt);
-        }
-
-        return credential;
+        return _credentialRepository.Get(id, cancellationToken);
     }
 }
