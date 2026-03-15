0cd2645f6780609db71417975887241bc6bf2aa6
LinuxChata <linuxchata@hotmail.com>
2026-03-15 20:47:21 +0100

    Update Google URL in security configuration (#118)
    Update Google URL in security configuration

--- src/Shark.Fido2.Portal/Middleware/SecurityHeadersMiddleware.cs
+++ src/Shark.Fido2.Portal/Middleware/SecurityHeadersMiddleware.cs
@@ -7,7 +7,7 @@ public class SecurityHeadersMiddleware(RequestDelegate next)
     public async Task InvokeAsync(HttpContext context)
     {
         context.Response.Headers.Append("X-Frame-Options", "DENY");
-        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self' https://www.googletagmanager.com; img-src 'self' data: https://www.googletagmanager.com https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com; form-action 'self'; frame-ancestors 'none';");
+        context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self' https://www.googletagmanager.com; img-src 'self' data: https://www.googletagmanager.com https://*.google-analytics.com; connect-src 'self' https://*.google-analytics.com; form-action 'self'; frame-ancestors 'none';");
         context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
         context.Response.Headers.Append("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
         context.Response.Headers.Append("Referrer-Policy", "no-referrer");
