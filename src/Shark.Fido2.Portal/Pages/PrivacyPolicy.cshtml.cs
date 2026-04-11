81982c1bdc6db36ca422850eba9cd7ae4f79c01f
LinuxChata <linuxchata@hotmail.com>
2026-04-11 08:55:32 +0200

    Add legal pages (#125)
    Add legal pages

---
+++ src/Shark.Fido2.Portal/Pages/PrivacyPolicy.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shark.Fido2.Portal.Pages;
+
+public class PrivacyPolicyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ src/Shark.Fido2.Portal/Pages/TermsOfService.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shark.Fido2.Portal.Pages;
+
+public class TermsOfServiceModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
