74c21e0547777c53239d8435d1e78bf0122a32ad
linuxchata <linuxchata@hotmail.com>
2026-04-19 09:36:42 +0200

    Update Shark.Fido2 packages

--- src/Shark.Fido2.Portal/Controllers/AttestationController.cs
+++ src/Shark.Fido2.Portal/Controllers/AttestationController.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using System.Text.Json;
+﻿using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Shark.Fido2.Core.Abstractions;
 using Shark.Fido2.Domain.Options;
--- src/Shark.Fido2.Portal/Middleware/SecurityHeadersMiddleware.cs
+++ src/Shark.Fido2.Portal/Middleware/SecurityHeadersMiddleware.cs
@@ -4,7 +4,7 @@ public class SecurityHeadersMiddleware(RequestDelegate next)
 {
     private readonly RequestDelegate _next = next;
 
-    public async Task InvokeAsync(HttpContext context)
+    public Task InvokeAsync(HttpContext context)
     {
         context.Response.Headers.Append("X-Frame-Options", "DENY");
         context.Response.Headers.Append("Content-Security-Policy", "default-src 'self'; script-src 'self' https://www.googletagmanager.com; img-src 'self' data: https://www.googletagmanager.com https://*.google-analytics.com; connect-src 'self' https://*.google-analytics.com; form-action 'self'; frame-ancestors 'none';");
@@ -12,6 +12,6 @@ public class SecurityHeadersMiddleware(RequestDelegate next)
         context.Response.Headers.Append("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
         context.Response.Headers.Append("Referrer-Policy", "no-referrer");
 
-        await _next(context);
+        return _next(context);
     }
 }
